efc62f2a3f96745836940eaa7ad03939e4665443
Pranav Behal <pranavbehal09@gmail.com>
2022-02-20 14:22:53 -0500

    Progression

--- TSA Website Design/script.js
+++ TSA Website Design/script.js
@@ -1,9 +1,23 @@
 "use strict";
 
-let formCounter = 538;
+// formSubmitButton.addEventListener("click", () => {
+//   document.querySelector(#form-submitted-number)=formCounter++;
+// });
 
-let formSubmitButton = document.getElementsByClassName("submit-form");
+let formSubmittedNumber;
 
-formSubmitButton.addEventListener("click", () => {
-  document.querySelector(#form-submitted-number)=formCounter++;
-});
+window.onload = function getFormNumber() {
+  let formSubmittedNumber = document.getElementById("form-submitted-number");
+  return formSubmittedNumber;
+};
+
+function getFormNumber() {
+  let formSubmittedNumber = document.querySelector("#form-submitted-number");
+  // document.querySelector('#form-submitted-number').textContent = formSubmittedNumber++;
+  // return formSubmittedNumber;
+}
+
+document.querySelector("#form-submitted-number").textContent =
+  formSubmittedNumber++;
+
+console.log(getFormNumber());
